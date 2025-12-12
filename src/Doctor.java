a4588551c9e94365707e158ec5795fff472eca78
Meirkhan <m@Meirkhans-MacBook-Air.local>
2025-12-12 21:03:01 +0500

    updated

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,2 +1,67 @@
-public class Doctor {
+public class Doctor{
+    private int id;
+    private String name;
+    private String title;
+    private int experience;
+    private String state;
+
+    public Doctor(int id , String name , String title , int experience){
+        this.id = id;
+        this.name = name;
+        this.title = title;
+        this.experience = experience;
+    }
+
+    public int getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public int getExperience(){
+        return experience;
+    }
+    public String getState(){
+        return state;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public void setExperience(int experience){
+        this.experience = experience;
+    }
+    public void setState(String state){
+        this.state = state;
+    }
+
+    public boolean titleCheck(String title){
+        if(this.title == title){
+            return true;
+        }
+        return false;
+    }
+
+    public String whichMastery(){
+        if(this.experience <= 5){
+            return "Begginner";
+        }
+        if(this.experience <= 10){
+            return "Medium";
+        }
+        return "Master";
+    }
+
+    public String toString(){
+        return "Doctor{ID = " + id + " , name = '" + name + "' , title = '" + title + "' , experience = " + experience + " , state = '" + state + "' }";
+    }
 }
--- src/Patient.java
+++ src/Patient.java
@@ -1,2 +1,68 @@
 public class Patient {
+    private String name;
+    private String contact;
+    private int age;
+    private String disease;
+    private int id;
+    private int points;
+
+    public Patient(String name, String contact, int age, String disease, int id , int points) {
+        this.name = name;
+        this.contact = contact;
+        this.age = age;
+        this.disease = disease;
+        this.id = id;
+        this.points = points;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public String getContact(){
+        return contact;
+    }
+    public int getAge(){
+        return age;
+    }
+    public String getDisease(){
+        return disease;
+    }
+    public int getId(){
+        return id;
+    }
+    public int getPoints(){
+        return points;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setContact(String contact){
+        this.contact = contact;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public void setDisease(String disease){
+        this.disease = disease;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setPoints(int points){
+        this.points = points;
+    }
+
+    public void addLoyaltyPoints(int points){
+        this.points += points;
+    }
+    public boolean CheckLoyalty() {
+        if (this.points >= 100) {
+            return true;
+        }
+        return false;
+    }
+    public String toString(){
+        return "Patient{ID = ' + id + ' , name = '" + name + "' , age = " + age + " , disease = '" + disease + "' , points = " + points + " , contact = " + contact + "}";
+    }
 }
